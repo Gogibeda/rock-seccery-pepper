f60a3d1425630d99b217040efcfd057e17e1a3aa
Administrator <Administrator@GOGA>
2023-01-17 13:22:37 +0400

    Add project files.

---
+++ rock seccery pepper/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rock_seccery_pepper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int computerScore = 0;
+            int playerScore = 0;
+            string[] move = { "rock", "scissor", "peper" };
+
+            int rock = 1;
+            int scissor = 2;
+            int pepper = 3;
+            
+
+            while (computerScore != 3 && playerScore != 3)
+            {
+                Console.WriteLine("Enter  rock , scissor or pepper ");
+                string playerMove = Console.ReadLine();
+                
+                if (playerMove != "rock" && playerMove != "scissor" && playerMove != "pepper")
+                {
+                    Console.WriteLine("Wrong input ");
+                }
+                
+                var random = new Random();
+                int computerNumber= random.Next(0,3);
+                string computerMove = computerNumber.ToString();
+                if (playerMove == "rock" && computerNumber == 2 ||  playerMove == "scissor" && computerNumber == 3 || playerMove == "pepper" && computerNumber == 1)
+                {
+                    Console.WriteLine("Player wins");
+                    playerScore++;
+                }
+                else if (playerMove == computerMove)
+                {
+                    Console.WriteLine("This is draw , Try again");
+                }
+                else
+                {
+                    Console.WriteLine("Computer win");
+                    computerScore++;
+                }
+                Console.WriteLine($"computer's score: {computerScore}");
+                Console.WriteLine($"player's score: {playerScore}");
+
+
+
+
+
+
+            }
+            Console.WriteLine(playerScore == 3 ? "player wins!" : "Computer wins!");
+        }
+    }
+}
---
+++ rock seccery pepper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("rock seccery pepper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("rock seccery pepper")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("081b7f1a-e378-4cb2-80f6-3d715ef4ff99")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
